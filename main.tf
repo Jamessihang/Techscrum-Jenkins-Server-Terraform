ac021814e810e3003c26461914afe2aaaa136529
James Liu <jamessihang@hotmail.com>
2022-12-12 00:08:54 +1100

    update

--- main.tf
+++ main.tf
@@ -122,43 +122,6 @@ resource "aws_instance" "ec2_instance" {
   }
 }
 
-#Create a role
-resource "aws_iam_role" "ec2_role" {
-  name = "ec2_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-#Get ploicy information
-data "aws_iam_policy" "ec2" {
-  arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-#Attach role to policy
-resource "aws_iam_policy_attachment" "ec2_policy_role" {
-  name       = "ec2_attachment"
-  roles      = [aws_iam_role.ec2_role.name]
-  policy_arn = data.aws_iam_policy.ec2.arn
-}
-
-#Attach role to an instance profile
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.ec2_role.name
-}
-
 # an empty resource block
 resource "null_resource" "name" {
 
