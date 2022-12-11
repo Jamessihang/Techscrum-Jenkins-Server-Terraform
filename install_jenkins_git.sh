c369399e00a2354ad0a40b006d3cd1c40253953e
James Liu <jamessihang@hotmail.com>
2022-12-11 14:04:26 +1100

    update files

--- install_jenkins_git.sh
+++ install_jenkins_git.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 sudo yum update –y
+#Search for Docker package:
+sudo yum search docker
+#Get version information:
+sudo yum info docker
+#Install docker
+sudo yum install docker
+#Add group membership for the default ec2-user so you can run all docker commands without using the sudo command:
+sudo usermod -a -G docker ec2-user
+id ec2-user
+# Reload a Linux user's group assignments to docker w/o logout
+newgrp docker
+# If need docker-compose too:
+# 1. Get pip3 
+#sudo yum install python3-pip
+ # 2. Then run any one of the following
+#sudo pip3 install docker-compose # with root access
+#Enable docker service at AMI boot time:
+sudo systemctl enable docker.service
+#Start the Docker service:
+sudo systemctl start docker.service
+
+
 #Install git in your EC2 instance
 sudo yum install git -y
 #Check git version
