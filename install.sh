8058148947e8f261e31871b9885ef14d8d77d5fa
James Liu <jamessihang@hotmail.com>
2022-12-20 13:19:15 +1100

    update

--- install.sh
+++ install.sh
@@ -17,11 +17,11 @@ sudo systemctl enable docker.service
 sudo systemctl start docker.service
 
 #Install yum-config-manager to manage your repositories.
-sudo yum install -y yum-utils
+#sudo yum install -y yum-utils
 #Use yum-config-manager to add the official HashiCorp Linux repository.
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+#sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 #Install Terraform from the new repository.
-sudo yum -y install terraform
+#sudo yum -y install terraform
 
 #Install git in your EC2 instance
 sudo yum install git -y
