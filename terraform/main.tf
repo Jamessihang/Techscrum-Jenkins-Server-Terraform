0f29a003d0c3bb203b4d8147b3ff44fa29b5ac86
James Liu <jamessihang@hotmail.com>
2022-12-27 19:33:02 +1100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -120,7 +120,7 @@ data "aws_ami" "amazon_linux_2" {
 # launch the ec2 instance
 resource "aws_instance" "jenkins_instance" {
   ami                    = data.aws_ami.amazon_linux_2.id
-  instance_type          = "t2.small"
+  instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.jenkins_security_group.id]
   key_name               = "Jenkins_key"
